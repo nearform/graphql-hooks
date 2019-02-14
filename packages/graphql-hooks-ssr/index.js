1a5f6967dbd61fcaeec7cdfa034308a2c3cd514a
Brian Mullan <bmullan91@gmail.com>
2019-02-14 14:02:39 +0000

    first

---
+++ packages/graphql-hooks-ssr/index.js
@@ -0,0 +1,22 @@
+const ReactDOMServer = require('react-dom/server');
+
+async function getInitialState(opts) {
+  const { App, client, render = ReactDOMServer.renderToStaticMarkup } = opts;
+  // ensure ssrMode is set:
+  client.ssrMode = true;
+  render(App);
+
+  if (client.ssrPromises.length) {
+    await Promise.all(client.ssrPromises);
+    // clear promises
+    client.ssrPromises = []
+    // recurse there may be dependant queries
+    return getInitialState(opts);
+  } else {
+    return client.cache.getInitialState();
+  }
+}
+
+module.exports = {
+  getInitialState
+};
