7cf80c8b8a75d6390f9e7d71c3612829854527c7
Jack Clark <jack.clark@nearform.com>
2019-02-20 10:16:28 +0000

    unit tests for useManualQuery

---
+++ test/unit/useManualQuery.test.js
@@ -0,0 +1,23 @@
+import { useManualQuery, useClientRequest } from '../../src';
+
+jest.mock('../../src/useClientRequest');
+
+const TEST_QUERY = `query Test($limit: Int) {
+  tests(limit: $limit) {
+    id
+  }
+}`;
+
+describe('useManualQuery', () => {
+  afterEach(() => {
+    jest.unmock('../../src/useClientRequest');
+  });
+
+  it('calls useClientRequest with useCache set to true & options', () => {
+    useManualQuery(TEST_QUERY, { option: 'option' });
+    expect(useClientRequest).toHaveBeenCalledWith(TEST_QUERY, {
+      useCache: true,
+      option: 'option'
+    });
+  });
+});
