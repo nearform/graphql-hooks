eea43c99c323dccd442de2f22e6cef43df392389
Jack Clark <jack.clark@nearform.com>
2019-03-04 15:10:17 +0000

    prettier babel config

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
-          node: "current"
+          node: 'current'
         }
       }
     ],
-    "@babel/preset-react"
+    '@babel/preset-react'
   ]
 };
