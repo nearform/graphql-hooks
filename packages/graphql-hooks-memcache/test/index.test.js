bc3854e20c809dec596fe95964452415319d687a
Joe Warren <Joezo@users.noreply.github.com>
2019-03-11 17:14:16 +0000

    test: Add test for graphql-hooks-memcache (#122)
    ### What does this PR do?
    
    Adds a test for the memcache package.
    
    ### Related issues
    
    closes #93 
    
    ### Checklist
    
    - [x] I have checked the [contributing document](../blob/master/CONTRIBUTING.md)
    - [x] I have added or updated any relevant documentation
    - [x] I have added or updated any relevant tests

---
+++ packages/graphql-hooks-memcache/test/index.test.js
@@ -0,0 +1,38 @@
+import memCache from '../src'
+
+describe('memcache', () => {
+  let cache
+  beforeEach(() => {
+    cache = memCache()
+  })
+  it('sets and gets key with string key', () => {
+    cache.set('foo', 'bar')
+    expect(cache.get('foo')).toEqual('bar')
+  })
+  it('sets and gets key with object key', () => {
+    cache.set({ foo: 'foo' }, 'baz')
+    expect(cache.get({ foo: 'foo' })).toEqual('baz')
+  })
+  it('deletes a key', () => {
+    cache.set('foo', 'baz')
+    expect(cache.get('foo')).toEqual('baz')
+    cache.delete('foo')
+    expect(cache.get('foo')).toBe(undefined)
+  })
+  it('lists all keys', () => {
+    cache.set('foo', 'bar')
+    cache.set('bar', 'baz')
+    expect(cache.keys().length).toEqual(2)
+  })
+  it('clears all keys', () => {
+    cache.set('foo', 'bar')
+    cache.set('bar', 'baz')
+    expect(cache.keys().length).toEqual(2)
+    cache.clear()
+    expect(cache.keys().length).toEqual(0)
+  })
+  it('returns initial state', () => {
+    cache = memCache({ initialState: { foo: 'bar' } })
+    expect(cache.getInitialState()).toEqual({ foo: 'bar' })
+  })
+})
