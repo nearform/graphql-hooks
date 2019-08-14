cb4f4f8ecdaee9a48a99261c35f4be4b8bc9fb94
Gal Dubitski <galmail@gmail.com>
2019-08-14 16:56:34 +0100

    testing command

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-packages_checksum=$(echo $(checksum "package.json") | head -n1 | awk '{print $1;}')
+packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    packages_checksum+=_$(echo $(checksum $file) | head -n1 | awk '{print $1;}')
+    packages_checksum+=_$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
 done
+export GRAPHQL_HOOKS_CHECKSUM=$packages_checksum
 echo $packages_checksum
