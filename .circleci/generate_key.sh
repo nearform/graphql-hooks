ddc3767c84e426509936b5af1e9d0666f8a34b4d
Gal Dubitski <galmail@gmail.com>
2019-08-15 12:21:37 +0100

    unique key generated for all packages.json

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    packages_checksum+=_$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
+    packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
 done
-export GRAPHQL_HOOKS_CHECKSUM=$packages_checksum
 echo $packages_checksum
