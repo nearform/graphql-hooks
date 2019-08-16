556cfadb019da6e3d9ea8be6bd149565acada687
Gal Dubitski <gal@gals-mbp.home>
2019-08-16 11:05:17 +0100

    removed key generation script

--- .circleci/generate_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-all_packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
-for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    all_packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
-done
-echo $all_packages_checksum
