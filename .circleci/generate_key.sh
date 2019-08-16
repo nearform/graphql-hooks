c681af9345c8e62504a5f8285dbfba74257a08cf
Gal Dubitski <gal@gals-mbp.home>
2019-08-16 10:15:27 +0100

    using md5sum command

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-all_packages_checksum=$(echo $(checksum "package.json") | head -n1 | awk '{print $1;}')
+all_packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    all_packages_checksum+=-$(echo $(checksum $file) | head -n1 | awk '{print $1;}')
+    all_packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
 done
 echo $all_packages_checksum
