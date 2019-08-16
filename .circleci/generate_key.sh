904d3c65fb95ef66da7f821b2a08b69e9df4be75
Gal Dubitski <gal@gals-mbp.home>
2019-08-16 10:05:37 +0100

    using env var instead

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -3,11 +3,13 @@ new_packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
     new_packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
 done
-all_packages_checksum=$(<.all_packages_checksum)
-if [ $all_packages_checksum != $new_packages_checksum ]
-then
-    echo "Generating new key: $new_packages_checksum"
-    echo $new_packages_checksum > .all_packages_checksum
-else
-    echo "Using existing key: $all_packages_checksum"
-fi
+export GRAPHQL_HOOKS_CHECKSUM=$new_packages_checksum
+
+# all_packages_checksum=$(<.all_packages_checksum)
+# if [ $all_packages_checksum != $new_packages_checksum ]
+# then
+#     echo "Generating new key: $new_packages_checksum"
+#     echo $new_packages_checksum > .all_packages_checksum
+# else
+#     echo "Using existing key: $all_packages_checksum"
+# fi
