2330009b146a76b2e2879293b1e46df99ec8f646
Gal Dubitski <gal@gals-mbp.home>
2019-08-16 10:14:50 +0100

    fixed env var

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-new_packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
+all_packages_checksum=$(echo $(checksum "package.json") | head -n1 | awk '{print $1;}')
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    new_packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
+    all_packages_checksum+=-$(echo $(checksum $file) | head -n1 | awk '{print $1;}')
 done
-export GRAPHQL_HOOKS_CHECKSUM=$new_packages_checksum
-
-# all_packages_checksum=$(<.all_packages_checksum)
-# if [ $all_packages_checksum != $new_packages_checksum ]
-# then
-#     echo "Generating new key: $new_packages_checksum"
-#     echo $new_packages_checksum > .all_packages_checksum
-# else
-#     echo "Using existing key: $all_packages_checksum"
-# fi
+echo $all_packages_checksum
