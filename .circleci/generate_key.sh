8047ca872ef0a1d822aa23fcc9df8fd1390d0da1
Gal Dubitski <gal@gals-mbp.home>
2019-08-16 09:46:59 +0100

    refactored key generation script

--- .circleci/generate_key.sh
+++ .circleci/generate_key.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
+new_packages_checksum=$(echo $(md5sum "package.json") | head -n1 | awk '{print $1;}')
 for file in $(find ./packages -maxdepth 2 -name "package.json") ; do
-    packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
+    new_packages_checksum+=-$(echo $(md5sum $file) | head -n1 | awk '{print $1;}')
 done
-echo $packages_checksum
+all_packages_checksum=$(<.all_packages_checksum)
+if [ $all_packages_checksum != $new_packages_checksum ]
+then
+    echo "Generating new key: $new_packages_checksum"
+    echo $new_packages_checksum > .all_packages_checksum
+else
+    echo "Using existing key: $all_packages_checksum"
+fi
