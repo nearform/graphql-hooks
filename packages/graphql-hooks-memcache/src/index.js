e501584f4b3461b54954ac535b5e011eab7b16fe
Fabien Le Frapper <contact@fabienlefrapper.me>
2019-09-26 13:11:33 +0200

    Replace quick-lru with lru-cache

--- packages/graphql-hooks-memcache/src/index.js
+++ packages/graphql-hooks-memcache/src/index.js
@@ -1,12 +1,12 @@
 import fnv1a from '@sindresorhus/fnv1a'
-import QuickLRU from 'quick-lru'
+import LRU from 'lru-cache'
 
 function generateKey(keyObj) {
   return fnv1a(JSON.stringify(keyObj)).toString(36)
 }
 
-export default function memCache({ size = 100, initialState } = {}) {
-  const lru = new QuickLRU({ maxSize: size })
+export default function memCache({ size = 100, ttl = 0, initialState } = {}) {
+  const lru = new LRU({ maxSize: size, maxAge: ttl })
 
   if (initialState) {
     Object.keys(initialState).map(k => {
