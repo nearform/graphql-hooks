2aa8ccac92da0ded244ae8c829c1bd3a601616a3
Brian Mullan <bmullan91@gmail.com>
2020-01-21 14:25:32 +0000

    fix: memcache IE11 support fixes #331

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GraphQLClient, ClientContext } from 'graphql-hooks'
-
+import memCache from 'graphql-hooks-memcache'
 import Posts from './components/Posts'
 
 const client = new GraphQLClient({
+  cache: memCache(),
   url: 'https://api.graph.cool/simple/v1/cjs4qo29b2w0c0130tfx6maca'
 })
 
 export default function App() {
+  const [showPosts, setShowPosts] = useState(true)
+
+  const togglePosts = () => setShowPosts(!showPosts)
+
   return (
     <ClientContext.Provider value={client}>
       <h1>Postie</h1>
-      <Posts />
+      <span>Verify caching by hiding/showing the posts: </span>
+      <button onClick={togglePosts}>{showPosts ? 'hide' : 'show'}</button>
+      {showPosts && <Posts />}
     </ClientContext.Provider>
   )
 }
