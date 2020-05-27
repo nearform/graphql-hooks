d8e24e966f8d46b89e2fed81a32b9cdbabc90351
Gal Dubitski <gal.dubitski@nearform.com>
2020-05-27 16:17:25 +0100

    chore: updating prettier (#514)

--- packages/graphql-hooks/index.d.ts
+++ packages/graphql-hooks/index.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { SubscriptionClient } from 'subscriptions-transport-ws'
 
 // Exports
 
@@ -62,15 +62,31 @@ export function useQuery<
   options?: UseQueryOptions<Variables>
 ): UseQueryResult<ResponseData, Variables, TGraphQLError>
 
-export function useManualQuery<ResponseData = any, Variables = object, TGraphQLError = object>(
+export function useManualQuery<
+  ResponseData = any,
+  Variables = object,
+  TGraphQLError = object
+>(
   query: string,
   options?: UseClientRequestOptions<Variables>
-): [FetchData<ResponseData, Variables, TGraphQLError>, UseClientRequestResult<ResponseData, TGraphQLError>, ResetFunction]
+): [
+  FetchData<ResponseData, Variables, TGraphQLError>,
+  UseClientRequestResult<ResponseData, TGraphQLError>,
+  ResetFunction
+]
 
-export function useMutation<ResponseData = any, Variables = object, TGraphQLError = object>(
+export function useMutation<
+  ResponseData = any,
+  Variables = object,
+  TGraphQLError = object
+>(
   query: string,
   options?: UseClientRequestOptions<Variables>
-): [FetchData<ResponseData, Variables, TGraphQLError>, UseClientRequestResult<ResponseData, TGraphQLError>, ResetFunction]
+): [
+  FetchData<ResponseData, Variables, TGraphQLError>,
+  UseClientRequestResult<ResponseData, TGraphQLError>,
+  ResetFunction
+]
 
 export interface SubscriptionRequest {
   query: string
