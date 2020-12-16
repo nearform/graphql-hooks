2388926c905e3098338176a5e9d712094f283976
Simone Busoli <simone.busoli@nearform.com>
2020-12-16 22:54:01 +0100

    fix: remove Buffer support from fnv1a (#609)

--- packages/graphql-hooks-memcache/test/fnv1a.test.js
+++ packages/graphql-hooks-memcache/test/fnv1a.test.js
@@ -45,29 +45,6 @@ describe('fnv1a tests', () => {
     ).toEqual(2964896417)
   })
 
-  it('works for buffer', () => {
-    expect(fnv1a(Buffer.from(''))).toEqual(2166136261)
-    expect(fnv1a(Buffer.from('🦄🌈'))).toEqual(2868248295)
-    expect(fnv1a(Buffer.from('h'))).toEqual(3977000791)
-    expect(fnv1a(Buffer.from('he'))).toEqual(1547363254)
-    expect(fnv1a(Buffer.from('hel'))).toEqual(179613742)
-    expect(fnv1a(Buffer.from('hell'))).toEqual(477198310)
-    expect(fnv1a(Buffer.from('hello'))).toEqual(1335831723)
-    expect(fnv1a(Buffer.from('hello '))).toEqual(3801292497)
-    expect(fnv1a(Buffer.from('hello w'))).toEqual(1402552146)
-    expect(fnv1a(Buffer.from('hello wo'))).toEqual(3611200775)
-    expect(fnv1a(Buffer.from('hello wor'))).toEqual(1282977583)
-    expect(fnv1a(Buffer.from('hello worl'))).toEqual(2767971961)
-    expect(fnv1a(Buffer.from('hello world'))).toEqual(3582672807)
-    expect(
-      fnv1a(
-        Buffer.from(
-          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.'
-        )
-      )
-    ).toEqual(2964896417)
-  })
-
   it("can't hash objects", () => {
     expect(() => fnv1a({})).toThrow(Error)
   })
