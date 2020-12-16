54ae7b5cd96c13f3b19a6aa62abfef8014a1ae14
Simone Busoli <simone.busoli@gmail.com>
2020-12-16 20:02:08 +0100

    fix: remove Buffer support from fnv1a

--- packages/graphql-hooks-memcache/src/fnv1a.js
+++ packages/graphql-hooks-memcache/src/fnv1a.js
@@ -37,26 +37,10 @@ function fnv1aString(string) {
   return hash >>> 0
 }
 
-function fnv1aBuffer(buf) {
-  let hash = OFFSET_BASIS_32
-
-  for (let i = 0; i < buf.length; ) {
-    hash ^= buf[i++]
-
-    // 32-bit FNV prime: 2**24 + 2**8 + 0x93 = 16777619
-    // Using bitshift for accuracy and performance. Numbers in JS suck.
-    hash += (hash << 1) + (hash << 4) + (hash << 7) + (hash << 8) + (hash << 24)
-  }
-
-  return hash >>> 0
-}
-
 export default function fnv1a(input) {
-  if (input instanceof Buffer) {
-    return fnv1aBuffer(input)
-  } else if (typeof input === 'string') {
+  if (typeof input === 'string') {
     return fnv1aString(input)
   } else {
-    throw new Error('input must be a string or a Buffer')
+    throw new Error('input must be a string')
   }
 }
