16a5ecab7904c430e04fefc430cca0ff6ddbecbe
Václav Zeman <vaclav.zeman@outlook.com>
2022-03-09 08:58:25 +0100

    fix: remove unused dependency

--- packages/graphql-hooks/src/Middleware.js
+++ packages/graphql-hooks/src/Middleware.js
@@ -13,6 +13,14 @@ export default class Middleware {
     })(this.run)
   }
 
+  /**
+   * Run middleware
+   * @param {opts.client} GraphQLClient instance
+   * @param {opts.operation} Operation object with properties such as query and variables
+   * @param {opts.resolve} Used to early resolve the request
+   * @param {opts.reject} User to early reject the request
+   * @param {function} next
+   */
   run(opts, next) {
     next.apply(this, opts)
   }
