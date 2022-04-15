499099f75363fa9be132a2f922d452f207b3f54e
Václav Zeman <vaclav.zeman@outlook.com>
2022-04-15 12:13:15 +0200

    Add APQ Middleware (#794)
    * feat: minimal wip middleware
    
    * feat: wip
    
    * fix: remove unused dependency
    
    * feat: add response hook
    
    * feat(apq): update middleware
    
    * test(apq): move and update tests
    
    * fix: dependency
    
    * docs: update readme
    
    * fix: fixed the error handling
    
    * Fix Usage formatting in READMEs
    
    * Update packages/graphql-hooks/src/middlewares/apqMiddleware.js
    
    Use built-in digest and Buffer.from methods
    
    Co-authored-by: Wilk Maia <wilkmaia@gmail.com>
    
    * Prefer @aws-crypto/sha256-universal
    
    * restore missing content
    
    Co-authored-by: Radomir Drndarski <radomir.drndarski@nearform.com>
    Co-authored-by: Kurt Milam <kurt.milam@xioup.com>
    Co-authored-by: Wilk Maia <wilkmaia@gmail.com>

--- packages/graphql-hooks/src/middlewares/apqMiddleware.js
+++ packages/graphql-hooks/src/middlewares/apqMiddleware.js
@@ -1,9 +1,12 @@
-async function sha256(data) {
-  // MOCK
-  return data
-    .split('')
-    .reduce((acc, ch) => acc + ch.charCodeAt(0), '')
-    .substr(0, 8)
+import { Sha256 } from '@aws-crypto/sha256-universal'
+
+export async function sha256(query) {
+  const hash = new Sha256()
+  hash.update(query, 'utf8')
+  const hashUint8Array = await hash.digest()
+
+  const hashBuffer = Buffer.from(hashUint8Array)
+  return hashBuffer.toString('hex')
 }
 
 /**
@@ -12,15 +15,24 @@ async function sha256(data) {
  * @returns Promise<object>
  */
 const APQMiddleware = async ({ operation, client, resolve, reject }, next) => {
+  const ERROR_PERSISTED_QUERY_NOT_FOUND = 'PERSISTED_QUERY_NOT_FOUND'
   try {
     // Add SHA256 of a query
-    operation = { ...operation, hash: await sha256(operation.query) }
+    operation.extensions = {
+      ...operation.extensions,
+      persistedQuery: {
+        version: 1,
+        sha256Hash: await sha256(operation.query)
+      }
+    }
 
     // Try to send just the hash
-    const res = await client.requestViaHttp(operation, {
-      fetchOptionsOverrides: { method: 'GET' },
-      hashOnly: true
-    })
+    const res = await client.requestViaHttp(
+      { ...operation, query: null },
+      {
+        fetchOptionsOverrides: { method: 'GET' }
+      }
+    )
 
     // Data fetched successfully -> resolve early
     if (!res.error) {
@@ -28,8 +40,19 @@ const APQMiddleware = async ({ operation, client, resolve, reject }, next) => {
     }
 
     const { error } = res
+
+    if (!error.fetchError && !error.graphQLErrors) {
+      throw error
+    }
+
+    const persistedQueryNotFound = error.fetchError
+      ? error.fetchError.type === ERROR_PERSISTED_QUERY_NOT_FOUND
+      : error.graphQLErrors.some(
+          gqError => gqError.extensions.code === ERROR_PERSISTED_QUERY_NOT_FOUND
+        )
+
     // If a server has not recognized the hash, send both query and hash
-    if (JSON.parse(error.fetchError).type === 'PERSISTED_QUERY_NOT_FOUND') {
+    if (persistedQueryNotFound) {
       next()
     } else {
       throw error
