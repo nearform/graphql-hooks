255892283f73ea199f9bd20a687e7595971d8205
Ryan Park <admin@ryanpark.co.uk>
2022-04-21 14:43:52 +0100

    add ts conversion for apq middleware

--- packages/graphql-hooks/rollup.config.js
+++ packages/graphql-hooks/rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel'
+import esbuild from 'rollup-plugin-esbuild' // Used for TS transpiling
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 
 import generateRollupConfig from '../../config/rollup.config'
@@ -7,11 +8,11 @@ export default generateRollupConfig({
   name: 'GraphQLHooks',
   entryPoint: 'src/index.ts'
 }).concat({
-  input: 'src/middlewares/apqMiddleware.js',
+  input: 'src/middlewares/apqMiddleware.ts',
   output: {
     file: `lib/middlewares/apqMiddleware.js`,
     format: 'cjs',
     indent: false
   },
-  plugins: [babel(), sizeSnapshot()]
+  plugins: [esbuild(), babel(), sizeSnapshot()]
 })
