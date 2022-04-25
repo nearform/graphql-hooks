d5c5286b1da19b7e526f921366e6f3ac491c4ea4
Ian Lovell <ian.lovell@nearform.com>
2022-04-25 16:23:22 +0100

    test: update config

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,6 @@ const projects = [
     ...{
       displayName: 'graphql-hooks',
       roots: ['./packages/graphql-hooks'],
-      testMatch: ['<rootDir>/packages/graphql-hooks/**/*.test.*'],
       setupFiles: ['<rootDir>/packages/graphql-hooks/test/setup.js'],
       automock: false
     }
@@ -23,16 +22,14 @@ const projects = [
     ...commonConfig,
     ...{
       roots: ['./packages/graphql-hooks-memcache'],
-      displayName: 'graphql-hooks-memcache',
-      testMatch: ['<rootDir>/packages/graphql-hooks-memcache/**/*.test.ts']
+      displayName: 'graphql-hooks-memcache'
     }
   },
   {
     ...commonConfig,
     ...{
       roots: ['./packages/graphql-hooks-ssr'],
-      displayName: 'graphql-hooks-ssr',
-      testMatch: ['<rootDir>/packages/graphql-hooks-ssr/**/*.test.*']
+      displayName: 'graphql-hooks-ssr'
     }
   },
   {
@@ -44,7 +41,6 @@ const projects = [
         '\\.jsx?$': 'babel-jest'
       },
       displayName: 'cra-example',
-      testMatch: ['<rootDir>/examples/create-react-app/**/*.test.ts(x)'],
       testEnvironment: 'jsdom'
     }
   }
