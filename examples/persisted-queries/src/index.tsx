725371eadf54c5ca33f137736dc06a5d2a4adff9
Ian Lovell <ian.lovell@nearform.com>
2022-04-28 15:37:13 +0100

    feat: update pq example

--- examples/persisted-queries/src/index.tsx
+++ examples/persisted-queries/src/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.render(<App />, document.getElementById('root'))
