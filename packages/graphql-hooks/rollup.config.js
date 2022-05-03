0526fc1da210122a8b629c4cc0273053f59b8f6e
Ian Lovell <ian.lovell@nearform.com>
2022-05-03 10:57:56 +0100

    chore: update rollup

--- packages/graphql-hooks/rollup.config.js
+++ packages/graphql-hooks/rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel'
 import esbuild from 'rollup-plugin-esbuild' // Used for TS transpiling
-import replace from 'rollup-plugin-replace'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 
 import generateRollupConfig from '../../config/rollup.config'
@@ -15,12 +14,5 @@ export default generateRollupConfig({
     format: 'cjs',
     indent: false
   },
-  plugins: [
-    esbuild(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    babel(),
-    sizeSnapshot()
-  ]
+  plugins: [esbuild(), babel(), sizeSnapshot()]
 })
