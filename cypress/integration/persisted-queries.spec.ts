7852cbb134bce4b35eb446e54069622bbaaefe07
Ian Lovell <ian.lovell@nearform.com>
2022-05-04 16:02:56 +0100

    test: update env

--- cypress/integration/persisted-queries.spec.ts
+++ cypress/integration/persisted-queries.spec.ts
@@ -3,7 +3,7 @@ describe('persisted-queries', () => {
     const x = 4
     const y = 5
 
-    cy.visit(`http://localhost:${Cypress.env('PORT') ?? 3000}`)
+    cy.visit(`http://localhost:${Cypress.env('CLIENT_PORT') ?? 3000}`)
     cy.findByLabelText('x').type(x.toString())
     cy.findByLabelText('y').type(y.toString())
     cy.findByRole('button', { name: /add/i }).click()
