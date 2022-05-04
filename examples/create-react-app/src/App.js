c2e4c68cacee31eb93a8b34239176c811e104f62
Ian Lovell <ian.lovell@nearform.com>
2022-05-04 09:06:15 +0100

    test: remove unused import

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 import { GraphQLClient, ClientContext } from 'graphql-hooks'
-// eslint-disable-next-line no-unused-vars
-import { APQMiddleware } from 'graphql-hooks/lib/middlewares/apqMiddleware'
 import memCache from 'graphql-hooks-memcache'
 
 import Posts from './components/Posts'
