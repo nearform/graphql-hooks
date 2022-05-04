5edf103459cfc49dce6f9cd91b5e2085728fff68
Ian Lovell <ian.lovell@nearform.com>
2022-05-04 13:57:06 +0100

    feat: add mercurius server

---
+++ examples/persisted-queries/server/mercurius.ts
@@ -0,0 +1,46 @@
+import fastify from 'fastify'
+import cors from '@fastify/cors'
+import mercurius from 'mercurius'
+
+const app = fastify({
+  logger: {
+    prettyPrint: true
+  }
+})
+
+const schema = `
+  type Query {
+  add(x: Int!, y: Int!): Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    add: async (_: unknown, obj: { x: number; y: number }) => {
+      const { x, y } = obj
+      return x + y
+    }
+  }
+}
+
+app.register(cors, {
+  origin: 'http://localhost:3002'
+})
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  persistedQueryProvider: mercurius.persistedQueryDefaults.automatic(),
+  graphiql: true
+})
+
+async function start() {
+  try {
+    await app.listen(8000)
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
