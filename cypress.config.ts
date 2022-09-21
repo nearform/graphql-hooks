1da1dce14747ad0bfacb32c730e4bf954933fda0
Radomir Drndarski <radomir.drndarski@nearform.com>
2022-09-21 14:44:34 +0200

    fix: updated cypress, updated cypress tsconfig and lib & target in the root tsconfig

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     defaultCommandTimeout: 6000,
-    video: false
+    video: false,
+    supportFile: "cypress/support/e2e.js",
   }
 })
