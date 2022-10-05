550f2fae6c7db894487c2754d222bf6e588c173b
Laszlo Mile <mlacif1@gmail.com>
2022-10-05 14:36:04 +0200

    fix: 930: use operation object in setQueryData and in invalidateQuery
    fix: 930: added test which checks variables operation property
    
    fix: 930: made setQueryData and invalidateQuery args dynamic
    
    fix: 930: fixed typo in readme

--- packages/graphql-hooks/test-node/GraphQLClient.test.ts
+++ packages/graphql-hooks/test-node/GraphQLClient.test.ts
@@ -4,6 +4,7 @@ import { FormData, File } from 'formdata-node'
 import { fileFromPathSync } from 'formdata-node/file-from-path'
 import { Events } from '../src/events'
 import EventEmitter from 'events'
+import { Operation } from '../lib/types/common-types'
 
 const validConfig = {
   url: 'https://my.graphql.api'
@@ -115,6 +116,112 @@ describe('with files in Node JS', () => {
     expect(requestMock).not.toBeCalled()
   })
 
+  it('correctly uses the invalidateQuery function when using an Operation object', () => {
+    const operation = {
+      query: validQuery,
+      operationName: 'Operation'
+    }
+    
+    const client = new GraphQLClient(validConfig)
+    const mutationsEmitterMock = {
+      ...new EventEmitter(),
+      emit: jest.fn()
+    }
+    const cacheMock = {
+      get: jest.fn(),
+      set: jest.fn(),
+      delete: jest.fn(),
+      clear: jest.fn(),
+      keys: jest.fn(),
+      getInitialState: jest.fn()
+    }
+    const getCacheKeyMock = jest.fn(query => query)
+    const resultMock = { hello: 'World' }
+
+    client.getCacheKey = getCacheKeyMock
+    client.cache = cacheMock
+    client.request = jest.fn(() => ({
+      then: jest.fn(callback => {
+        callback(resultMock)
+        return {
+          catch: jest.fn()
+        }
+      })
+    })) as any
+    client.mutationsEmitter = mutationsEmitterMock
+
+    client.invalidateQuery(operation)
+
+    expect(getCacheKeyMock).toBeCalledTimes(1)
+    expect(getCacheKeyMock).toBeCalledWith(operation)
+
+    expect(cacheMock.delete).toBeCalledTimes(1)
+    expect(cacheMock.delete).toBeCalledWith(operation)
+
+    expect(mutationsEmitterMock.emit).toBeCalledTimes(1)
+    expect(mutationsEmitterMock.emit).toBeCalledWith(
+      Events.DATA_INVALIDATED,
+      resultMock
+    )
+  })
+
+  it('should call invalidateQuery function with variables property set on the Operations object', () => {
+    const file = fileFromPathSync(path.resolve(__dirname, './sample.txt'))
+
+    const operation = {
+      operationName: 'Operation',
+      variables: {
+        a: file,
+        b: 123,
+        c: 'test',
+        d: true
+      },
+      query: validQuery
+    }
+
+    const client = new GraphQLClient(validConfig)
+    const mutationsEmitterMock = {
+      ...new EventEmitter(),
+      emit: jest.fn()
+    }
+    const cacheMock = {
+      get: jest.fn(),
+      set: jest.fn(),
+      delete: jest.fn(),
+      clear: jest.fn(),
+      keys: jest.fn(),
+      getInitialState: jest.fn()
+    }
+    const getCacheKeyMock = jest.fn(query => query)
+    const resultMock = { hello: 'World' }
+
+    client.getCacheKey = getCacheKeyMock
+    client.cache = cacheMock
+    client.request = jest.fn(() => ({
+      then: jest.fn(callback => {
+        callback(resultMock)
+        return {
+          catch: jest.fn()
+        }
+      })
+    })) as any
+    client.mutationsEmitter = mutationsEmitterMock
+
+    client.invalidateQuery(operation)
+
+    expect(getCacheKeyMock).toBeCalledTimes(1)
+    expect(getCacheKeyMock).toBeCalledWith(operation)
+
+    expect(cacheMock.delete).toBeCalledTimes(1)
+    expect(cacheMock.delete).toBeCalledWith(operation)
+
+    expect(mutationsEmitterMock.emit).toBeCalledTimes(1)
+    expect(mutationsEmitterMock.emit).toBeCalledWith(
+      Events.DATA_INVALIDATED,
+      resultMock
+    )
+  })
+
   it('should call every dependency of the setQueryData function', () => {
     const client = new GraphQLClient(validConfig)
     const stateMock = { hello: 'World' }
@@ -165,5 +272,111 @@ describe('with files in Node JS', () => {
     client.setQueryData(validQuery, updaterMock)
 
     expect(updaterMock).not.toBeCalled()
+  })
+
+  it('correctly sets the query data when operationName changes (using Operation object)', () => {
+    let operation = {
+      query: validQuery,
+      operationName: 'Operation1'
+    }
+
+    const client = new GraphQLClient(validConfig)
+    const stateMock = { hello: 'World' }
+    const olderStatateMock = { data: stateMock }
+    const mutationsEmitterMock = {
+      ...new EventEmitter(),
+      emit: jest.fn()
+    }
+    const cacheMock = {
+      get: jest.fn(() => olderStatateMock),
+      set: jest.fn(),
+      delete: jest.fn(),
+      clear: jest.fn(),
+      keys: jest.fn(),
+      getInitialState: jest.fn()
+    }
+    const getCacheKeyMock = jest.fn(query => query)
+    const resultMock = { hello: 'World' }
+    const updaterMock = jest.fn(() => resultMock)
+
+    client.getCacheKey = getCacheKeyMock
+    client.cache = cacheMock
+    client.mutationsEmitter = mutationsEmitterMock
+
+    client.setQueryData(operation, updaterMock)
+    expect(getCacheKeyMock).toBeCalledTimes(1)
+    expect(getCacheKeyMock).toBeCalledWith(operation)
+
+    operation.operationName = 'Operation2'
+
+    client.setQueryData(operation, updaterMock)
+
+    expect(getCacheKeyMock).toBeCalledTimes(2)
+    expect(getCacheKeyMock).toBeCalledWith(operation)
+
+    expect(cacheMock.get).toBeCalledTimes(2)
+    expect(cacheMock.get).toBeCalledWith(operation)
+
+    expect(updaterMock).toBeCalledTimes(2)
+    expect(updaterMock).toBeCalledWith(resultMock)
+
+    expect(mutationsEmitterMock.emit).toBeCalledTimes(2)
+    expect(mutationsEmitterMock.emit).toBeCalledWith(Events.DATA_UPDATED, {
+      data: resultMock
+    })
+  })
+
+  it('should call setQueryData function with variables property set on the Operations object', () => {
+    const file = fileFromPathSync(path.resolve(__dirname, './sample.txt'))
+
+    const operation = {
+      operationName: 'Operation',
+      variables: {
+        a: file,
+        b: 123,
+        c: 'test',
+        d: true
+      },
+      query: validQuery
+    }
+
+    const client = new GraphQLClient(validConfig)
+    const stateMock = { hello: 'World' }
+    const olderStatateMock = { data: stateMock }
+    const mutationsEmitterMock = {
+      ...new EventEmitter(),
+      emit: jest.fn()
+    }
+    const cacheMock = {
+      get: jest.fn(() => olderStatateMock),
+      set: jest.fn(),
+      delete: jest.fn(),
+      clear: jest.fn(),
+      keys: jest.fn(),
+      getInitialState: jest.fn()
+    }
+    const getCacheKeyMock = jest.fn(query => query)
+    const resultMock = { hello: 'World' }
+    const updaterMock = jest.fn(() => resultMock)
+
+    client.getCacheKey = getCacheKeyMock
+    client.cache = cacheMock
+    client.mutationsEmitter = mutationsEmitterMock
+
+    client.setQueryData(operation, updaterMock)
+
+    expect(getCacheKeyMock).toBeCalledTimes(1)
+    expect(getCacheKeyMock).toBeCalledWith(operation)
+
+    expect(cacheMock.get).toBeCalledTimes(1)
+    expect(cacheMock.get).toBeCalledWith(operation)
+
+    expect(updaterMock).toBeCalledTimes(1)
+    expect(updaterMock).toBeCalledWith(resultMock)
+
+    expect(mutationsEmitterMock.emit).toBeCalledTimes(1)
+    expect(mutationsEmitterMock.emit).toBeCalledWith(Events.DATA_UPDATED, {
+      data: resultMock
+    })
   })
 })
