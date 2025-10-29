624d46c9945a24867628adfd8132c23201811e29
Steve <srherzog@gmail.com>
2025-10-29 14:51:38 -0500

    chore: minor type fix

--- examples/persisted-queries/client/src/index.tsx
+++ examples/persisted-queries/client/src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />)
